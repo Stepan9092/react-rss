ddabd79a2f077a70549f218e020522a94f5f565f
Stepan Zubik <stteppa9092@gmail.com>
2023-04-02 01:32:43 +0300

    refactor: rewrite card, cardList components to functional

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Props } from './Props';
 import styles from './Card.module.css';
 
-export default class Card extends Component<Props> {
-  render() {
-    return (
-      <div className={styles.card}>
-        <div className={styles.field}>{this.props.item.name}</div>
-        <div className={styles.field}>{this.props.item.gender}</div>
-        <img src={this.props.item.photo} alt="photo" className={styles.photo} />
-        <div className={styles.field}>{this.props.item.birthday}</div>
-        <div className={styles.field}>{this.props.item.country}</div>
-      </div>
-    );
-  }
+export default function Card({ item }: Props) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.field}>{item.name}</div>
+      <div className={styles.field}>{item.gender}</div>
+      <img src={item.photo} alt="photo" className={styles.photo} />
+      <div className={styles.field}>{item.birthday}</div>
+      <div className={styles.field}>{item.country}</div>
+    </div>
+  );
 }
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,19 +1,17 @@
 import Card from '../Card/Card';
-import React, { Component } from 'react';
+import React from 'react';
 import { Props } from './Props';
 import styles from './CardList.module.css';
 
-export default class CardList extends Component<Props> {
-  render() {
-    return (
-      <div className={styles.container}>
-        <h2 className={styles.title}>Cards:</h2>
-        <div className={styles.list}>
-          {this.props.items.map((item) => (
-            <Card item={item} key={Date.now().toString() + item.birthday} />
-          ))}
-        </div>
+export default function CardList({ items }: Props) {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Cards:</h2>
+      <div className={styles.list}>
+        {items.map((item) => (
+          <Card item={item} key={Date.now().toString() + item.birthday} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
